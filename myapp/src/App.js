0c72ec01c309854097c3f94f01cc979d786cdca4
deva04-pvt <devanand.pvt.04@gmail.com>
2024-04-18 22:06:03 +0530

    Slider fixed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,12 +3,10 @@ import "./App.css";
 import USMap from "./components/USMap";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
-import RadioButtons from "./components/RadioButtons";
 const App = () => {
   return (
     <div>
     <Navbar/>
-    <RadioButtons/>
       <USMap/>
       <Slider/>
     </div>
--- myapp/src/components/Slider.js
+++ myapp/src/components/Slider.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Slider() {
+const Slider = () => {
+  const [value, setValue] = useState(50); // Initial value set to 50
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
-    <div className="py-8 px-4">
-      <label className="block text-gray-700">Slider</label>
-      <input type="range" min="0" max="100" className="w-full" />
+    <div className="w-64 mx-auto bg-gray-300 bg-opacity-50 p-4 rounded-lg">
+      <div className="flex items-center mb-2">
+        <span className="mr-2 font-semibold">Time Frame: (days)</span>
+        <output className="text-gray-700">{value}</output>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-2">0</span>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={value}
+          onChange={handleChange}
+          className="w-full h-4 bg-gray-200 rounded-full appearance-none focus:outline-none"
+        />
+        <span className="ml-2">100</span>
+      </div>
     </div>
   );
-}
+};
 
 export default Slider;
